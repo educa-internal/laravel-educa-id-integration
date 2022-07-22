a77bf1a385559bc633cc9b68629aa52b5d52a41b
Tuấn Hiệp <hieptt@educa.vn>
2022-07-22 09:57:49 +0700

    Feature/fix config

--- config/services.php
+++ config/services.php
@@ -6,9 +6,9 @@ return [
         'client_id' => env('TUTOR_ID_CLIENT_ID'),
         'client_secret' => env('TUTOR_ID_CLIENT_SECRET'),
         'redirect' => env('TUTOR_ID_REDIRECT'),
-        'url_authorize' => env('TUTOR_ID_URL_AUTHORIZE', 'http://lms-test.edupiatutor.vn/realms/Backend/protocol/openid-connect/auth'),
-        'url_get_token' => env('TUTOR_ID_URL_GET_TOKEN', 'http://lms-test.edupiatutor.vn/realms/Backend/protocol/openid-connect/token'),
-        'url_get_user_by_token' => env('TUTOR_ID_URL_GET_USER_BY_TOKEN', 'http://lms-test.edupiatutor.vn/realms/Backend/protocol/openid-connect/userinfo'),
-        'url_logout' => env('TUTOR_ID_URL_LOGOUT','http://lms-test.edupiatutor.vn/realms/Backend/protocol/openid-connect/logout')
+        'url_authorize' => env('TUTOR_ID_URL_AUTHORIZE'),
+        'url_get_token' => env('TUTOR_ID_URL_GET_TOKEN'),
+        'url_get_user_by_token' => env('TUTOR_ID_URL_GET_USER_BY_TOKEN'),
+        'url_logout' => env('TUTOR_ID_URL_LOGOUT')
     ]
 ];
