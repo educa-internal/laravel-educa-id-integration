4c61189bff2515b3ebfb81f339a414e659b76078
icovn <quanghuy.ico@gmail.com>
2022-07-22 09:59:44 +0700

    Update services.php

--- config/services.php
+++ config/services.php
@@ -4,6 +4,7 @@ return [
     'tutor-id' => [
         'diver_name' => env('TUTOR_ID_DIVER_NAME', 'tutor-id'),
         'client_id' => env('TUTOR_ID_CLIENT_ID'),
+        'client_secret' => env('TUTOR_ID_CLIENT_SECRET'),
         'redirect' => env('TUTOR_ID_REDIRECT'),
         'url_authorize' => env('TUTOR_ID_URL_AUTHORIZE'),
         'url_get_token' => env('TUTOR_ID_URL_GET_TOKEN'),
